53450a594bc7694325fe3dcdc3c78d8dc35fc384
Simon Jupp <simon.jupp@gmail.com>
2019-09-05 16:07:59 +0900

    added examples to schema, shema tests and travis config

--- build_ui.sh
+++ build_ui.sh
@@ -1,3 +1,3 @@
 cp ejp_vocabulary_context.json docs
 cp validation_vp_model/metadata_model.shex docs
-jsonschema2md -e json -d json_schema/ -n -o docs/ 
+jsonschema2md --p example e json -d json_schema/ -n -o docs/
